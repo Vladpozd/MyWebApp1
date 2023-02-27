f8675d613ffa18d13a9a423c04e7cee8eb056769
vladp <Vladpozdnakov35@gmail.com>
2023-02-27 10:33:22 +0500

    f

---
+++ src/test/java/com/company/UserRepositoryTests.java
@@ -0,0 +1,69 @@
+package com.company;
+
+import com.company.user.User;
+import com.company.user.UserRepository;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.Optional;
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+@Autowired private UserRepository repo;
+    private Object assertThat;
+
+    @Test
+    public void testAddNew(){
+    User user = new User();
+    user.setEmail("alex.stevenso@gmail.com");
+    user.setPassword("alex123456");
+    user.setFirstName("Alex");
+    user.setLastName("Stevenson");
+    User savedUser = repo.save(user);
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+}
+@Test
+    public void testListAll(){
+        Iterable <User> users = repo.findAll();
+        Assertions.assertThat(users).hasSizeGreaterThan(0);
+        for (User user : users){
+            System.out.println(user);
+        }
+}
+@Test
+    public void testUpdate(){
+        Integer userId = 1;
+    Optional<User> optionalUser= repo.findById(userId);
+    User user = optionalUser.get();
+    user.setPassword("hello2000");
+    repo.save(user);
+    User updateUser = repo.findById(userId).get();
+    Assertions.assertThat(updateUser.getPassword()).isEqualTo("hello2000");
+
+}
+@Test
+    public void testGet(){
+        Integer userId=2;
+        Optional<User> optionalUser=repo.findById(userId);
+       Assertions.assertThat(optionalUser).isPresent();
+    System.out.println(optionalUser.get());
+}
+@Test
+    public void testDelete(){
+        Integer userId=2;
+        repo.deleteById(userId);
+        Optional<User> optionalUser=repo.findById(userId);
+        Assertions.assertThat(optionalUser).isNotPresent();
+
+}
+}
